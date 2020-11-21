8deafe3ed727fc78ed27733b20f67482a28e50d9
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-11-20 22:25:43 -0800

    Took out stripe import

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE_PRIVATE_KEY);
+// const stripe = require('stripe')(process.env.REACT_APP_STRIPE_PRIVATE_KEY);
 
 const app = express();
 
@@ -22,28 +22,28 @@ var cert_url = '/etc/letsencrypt/live/servingfresh.me/cert.pem';
 
 var options = {};
 
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 0,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: 'https://example.com/success',
-    cancel_url: 'https://example.com/cancel',
-  });
+// app.post('/create-checkout-session', async (req, res) => {
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ['card'],
+//     line_items: [
+//       {
+//         price_data: {
+//           currency: 'usd',
+//           product_data: {
+//             name: 'T-shirt',
+//           },
+//           unit_amount: 0,
+//         },
+//         quantity: 1,
+//       },
+//     ],
+//     mode: 'payment',
+//     success_url: 'https://example.com/success',
+//     cancel_url: 'https://example.com/cancel',
+//   });
 
-  res.json({ id: session.id });
-});
+//   res.json({ id: session.id });
+// });
 
 if (process.env.SUDO_USER != undefined) {
   options['key'] = fs.readFileSync(key_url);
