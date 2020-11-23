f0f9a3f22aa07502c03e80463020ec2631402b73
quangdang87 <vinhquangdang1@gmail.com>
2020-11-22 18:31:15 -0800

    Finished refund tab for SF website. need to make the upload button look nicer

--- src/customer/checkoutPage/CheckoutRight.jsx
+++ src/customer/checkoutPage/CheckoutRight.jsx
@@ -1,46 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import CheckoutTab from './tabs/CheckoutTab';
-import appColors from '../../styles/AppColors';
-import { Box } from '@material-ui/core';
+import React, {useState, useEffect} from "react";
+import {makeStyles, withStyles} from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import CheckoutTab from "./tabs/CheckoutTab";
+import RefundTab from "./tabs/RefundTab";
+import appColors from "../../styles/AppColors";
+import {Box} from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
     backgroundColor: appColors.componentBg,
-    borderTopLeftRadius: 25,
-  },
+    borderTopLeftRadius: 25
+  }
 });
 
 const StyledTabs = withStyles({
   indicator: {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-    boxShadow: '0',
-    '& > span': {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    boxShadow: "0",
+    "& > span": {
       maxWidth: 40,
-      width: '100%',
-      backgroundColor: appColors.secondary,
-    },
-  },
-})((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+      width: "100%",
+      backgroundColor: appColors.secondary
+    }
+  }
+})(props => <Tabs {...props} TabIndicatorProps={{children: <span />}} />);
 
-const StyledTab = withStyles((theme) => ({
+const StyledTab = withStyles(theme => ({
   root: {
-    textTransform: 'none',
+    textTransform: "none",
     color: appColors.paragraphText,
     fontWeight: theme.typography.fontWeightRegular,
     fontSize: theme.typography.pxToRem(15),
     marginRight: theme.spacing(1),
-    '&:focus': {
-      opacity: 1,
-    },
-  },
-}))((props) => <Tab disableRipple {...props} />);
+    "&:focus": {
+      opacity: 1
+    }
+  }
+}))(props => <Tab disableRipple {...props} />);
 
 export default function CheckoutRight() {
   const classes = useStyles();
@@ -53,8 +54,8 @@ export default function CheckoutRight() {
   const [windowHeight, setWindowHeight] = React.useState(window.innerHeight);
 
   React.useEffect(() => {
-    window.addEventListener('resize', updateWindowHeight);
-    return () => window.removeEventListener('resize', updateWindowHeight);
+    window.addEventListener("resize", updateWindowHeight);
+    return () => window.removeEventListener("resize", updateWindowHeight);
   });
 
   const updateWindowHeight = () => {
@@ -62,22 +63,24 @@ export default function CheckoutRight() {
   };
 
   return (
-    <Paper className={classes.root} style={{ height: windowHeight - 85 }}>
+    <Paper className={classes.root} style={{height: windowHeight - 85}}>
       <StyledTabs
         value={value}
         onChange={handleChange}
-        indicatorColor="secondary"
-        textColor="secondary"
-        aria-label="styled tabs example"
+        indicatorColor='secondary'
+        textColor='secondary'
+        aria-label='styled tabs example'
         centered
       >
-        <StyledTab label="Checkout" />
+        <StyledTab label='Checkout' />
         <Box flexGrow={1} />
-        <StyledTab label="History" />
+        <StyledTab label='History' />
         <Box flexGrow={1} />
-        <StyledTab label="Refund" />
+        <StyledTab label='Refund' />
       </StyledTabs>
-      <CheckoutTab />
+      {value === 0 && <CheckoutTab />}
+      {value === 2 && <div> Hi, this is your History</div>}
+      {value === 4 && <RefundTab />}
     </Paper>
   );
 }
