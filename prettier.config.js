1a1a9b3259e198499d4c7243f91ebd1a54ddf487
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-12-01 13:45:35 -0800

    Create prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['*.jsx', '*.js'],
+      options: {
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+      },
+    },
+  ],
+};
