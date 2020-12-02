828c41eba5b4ba3befad173324f2db0e01c13b1b
quangdang87 <vinhquangdang1@gmail.com>
2020-12-01 23:01:16 -0800

    added Paypal.jsx

---
+++ src/customer/checkoutPage/tabs/Paypal.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+const PayPal = ({ value, setPaypal, setCartItems }) => {
+  const [loaded, setLoaded] = useState(false);
+  let paypalRef = useRef();
+
+  const CLIENT = {
+    sandbox: process.env.REACT_APP_PAYPAL_CLIENT_ID_TESTING,
+    production: process.env.REACT_APP_PAYPAL_CLIENT_ID_LIVE,
+  };
+
+  const CLIENT_ID =
+    process.env.NODE_ENV === 'production' ? CLIENT.production : CLIENT.sandbox;
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = `https://www.paypal.com/sdk/js?client-id=${CLIENT_ID}&currency=USD`;
+    script.addEventListener('load', () => setLoaded(true));
+    document.body.appendChild(script);
+
+    if (loaded) {
+      setTimeout(() =>
+        window.paypal
+          .Buttons({
+            createOrder: (data, actions) => {
+              return actions.order.create({
+                purchase_units: [
+                  {
+                    description: 'Testing',
+                    amount: {
+                      currency_code: 'USD',
+                      value: value,
+                    },
+                  },
+                ],
+              });
+            },
+            onApprove: async (data, actions) => {
+              const order = await actions.order.capture();
+              setPaypal(false);
+              setCartItems({});
+              console.log('order: ', order);
+            },
+          })
+          .render(paypalRef)
+      );
+    }
+  });
+  return (
+    <div>
+      <div ref={v => (paypalRef = v)} />
+    </div>
+  );
+};
+
+export default PayPal;
