7c80db9d0c6a7d3292be3adf63f3b5cd96e196de
Jeremy Manalo <jeremy.h.manalo@gmail.com>
2020-12-07 02:16:06 -0800

    fixed missing dependency

--- src/home/Landing.jsx
+++ src/home/Landing.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import {
