e9ae855b0ac1722a358b1cdb953b355b32c24deb
ParvaShah <parva.shah808@gmail.com>
2021-03-30 19:11:25 -0700

    default day selected

--- src/customer/productSelectionPage/filter/cards/DateCard.jsx
+++ src/customer/productSelectionPage/filter/cards/DateCard.jsx
@@ -54,7 +54,7 @@ const DateCard = (props) => {
   const store = useContext(storeContext);
   const confirm = useConfirmation();
 
-  const [isClicked, setIsClicked] = useState(true);
+  const [isClicked, setIsClicked] = useState(false);
   const [showCard, setShowCard] = useState(
     productSelect.farmsClicked.size == 0 ? true : false
   );
