6ac5100a4eebe9a7f606aa71c6977b0db6a45ab3
yash <yashosharma21@gmail.com>
2021-05-04 19:56:38 -0700

    not working home page

---
+++ src/home/Farmers.jsx
@@ -0,0 +1,199 @@
+import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { makeStyles } from '@material-ui/core/styles';
+import {Button} from 'react-bootstrap'
+import Card from "react-bootstrap/Card";
+import fb from '../icon/facebook.svg';
+import insta from '../icon/instagram.svg';
+
+
+import Carousel from 'react-multi-carousel';
+
+var responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 7
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+// var settings = {
+//   dots: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//       },
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//       },
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//       },
+//     },
+//   ],
+// };
+
+// const useStyles = makeStyles((theme) => ({
+//   Farmers: {
+//     // width: '800px',
+//     // margin: '30px',
+//     // border:'1px solid black'
+//   },
+//   imgcard: {
+//     //border: '1px solid #136D74',
+//     borderRadius: '14px',
+//     backgroundColor: '#F1F4F4',
+//     overflow: ' hidden',
+//     transition: '0.3s',
+//     padding: '60px',
+//     border:'1px solid black'
+//   },
+
+//   farmerimg: {
+//     width: '400px',
+//     height: '400px',
+//     border: '2px solid #136D74',
+//     // borderRadius: '10px',
+//     // marginLeft: '130px',
+//     // marginRight: '130px',
+//   },
+
+//   cardbody: {
+//     padding: '15px',
+//     //backgroundColor: '#ffffff',
+//     marginRight: '130px',
+//     marginLeft: '130px',
+//     //border: '1px solid #136D74',
+//     // borderRadius: '10px',
+//     marginLeft: '130px',
+//     border:'1px solid black'
+//   },
+//   cardname: {
+//     fontSize: '30px',
+//     fontWeight: '600',
+//     textAlign:'left',
+//     border:'1px solid black'
+//   },
+//   cardcity: {
+//     marginTop: '15px',
+//     fontSize: '20px',
+//     // width:'',
+//     textAlign:'left',
+//     color:'#236c73',
+//     border:'1px solid black'
+//     //textAlign: 'left',
+//     //marginLeft: '130px',
+//   },
+//   cardcontact: {
+//     fontSize: '20px',
+//     textAlign:'right'
+//     //textAlign: 'left',
+//     //marginLeft: '130px',
+//   },
+// }));
+export default function Farmers() {
+  
+ 
+  
+  const farm = () =>
+    [
+      {
+        farmerimg: './farmer1.jpg',
+        name: 'ABC Farm',
+        city: 'ABC',
+        contact: '123',
+      },
+      {
+        farmerimg: './farmer2.jpg',
+        name: 'DEF Farm',
+        city: 'DEF',
+        contact: '456',
+      },
+      {
+        farmerimg: './farmer3.jpg',
+        name: 'GHI Farm',
+        city: 'GHI',
+        contact: '789',
+      },
+      {
+        farmerimg: './farmer4.jpg',
+        name: 'IJK Farm',
+        city: 'IJK',
+        contact: '987',
+      },
+      {
+        farmerimg: './farmer5.jpg',
+        name: 'LMN Farms',
+        city: 'LMN',
+        contact: '654',
+      },
+      {
+        farmerimg: './farmer6.jpg',
+        name: 'OPQ Farms',
+        city: 'OPQ',
+        contact: '321',
+      },
+      {
+        farmerimg: './farmer7.jpg',
+        name: 'RST Farms',
+        city: 'RST',
+        contact: '147',
+      },
+      {
+        farmerimg: './farmer8.jpg',
+        name: 'UVW Farms',
+        city: 'UVW',
+        contact: '258',
+      },
+    ].map((x, i) => (
+      <div key="{i}" style={{ float:'left' , width: '20rem', margin:'10px', padding:'12px'}} className='classes.Farmers'>
+      <img  src={x.farmerimg} style={{width:'15rem',height:'15rem',border:'2px solid rgb(242,151,56)',aspectRatio:'1/1'}}/>
+      
+      <Card.Body>
+        
+      <div style={{color:'rgb(242,151,56)',fontWeight:'bold',fontSize:'25px',float:'left',width:'100%'}}><h3 style={{float:'left'}}>{x.name}</h3></div>
+      <div>
+      <div style={{float:'left',color:'rgb(35,109,115)',fontWeight:'bold',fontSize:'20px'}}>City:{x.city}</div>
+      <div style={{float:'right',padding:'2px',width:'20px',maxHeight:'10px'}}><img src={fb}></img></div>
+    <div style={{float:'right',padding:'2px'}}><img src={insta}></img></div>
+      </div>
+
+          </Card.Body>
+      </div>
+      
+    ));
+
+
+  return (
+    <Carousel responsive={responsive}>
+      {farm()}
+      </Carousel>
+
+
+);
+}
---
+++ src/home/SuccessModal.jsx
@@ -0,0 +1,31 @@
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useContext } from 'react';
+import classes from './ErrorModal.module.css';
+import {Button} from 'react-bootstrap';
+import Card from "react-bootstrap/Card";
+import cross from '../icon/cross.svg';
+
+const SuccessModal=(props)=>{
+    return(
+       <Card className={classes.modal} style={{borderRadius:'10px'}}>
+           <div >
+            <img src={cross} onClick={props.onConfirm} style={{float:'right',height:'25px',width:'25px',color:'black',marginTop:'3px',marginRight:'3px'}}></img>
+           </div>
+           <div>
+            <h2 style={{fontWeight:'bold'}}>{props.title}</h2>
+           </div>
+           <div style={{width:'300px'}}>
+           Looks like we deliver to your address. Click the button below to see the variety of fresh organic fruits and vegetables we offer.
+           </div>
+           <div >
+           <button onClick={props.onConfirm} style={{width:'250px',marginTop:'20px',marginBottom:'20px',height:'30px',borderRadius:'5px',backgroundColor:'#FF8500',color:'white',borderColor:'#E1E7E7'}}>Explore Local Produce</button>
+           </div>
+           <div>
+               <button onClick={props.onConfirm} style={{width:'250px',marginBottom:'20px',height:'30px',borderRadius:'5px',backgroundColor:'#FF8500',color:'white',borderColor:'#E1E7E7'}}>Sign Up</button>
+           </div>
+       </Card>
+    )
+}
+
+export default SuccessModal;
